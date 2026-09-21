f6f7304e10c881803d0efcbf379053a85d840191
Toortles <scmischler@outlook.com>
2026-09-21 14:04:15 -0700

    Initial Commit with boilerplate test GUI

---
+++ main.py
@@ -0,0 +1,12 @@
+import dearpygui.dearpygui as dpg
+
+dpg.create_context()
+dpg.create_viewport(title="Pyrrot", width=1280, height=800, resizable=False)
+
+with dpg.window(label="Test"):
+    dpg.add_text("Hello Window!")
+
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.start_dearpygui()
+dpg.destroy_context()
