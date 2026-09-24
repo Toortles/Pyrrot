d80fc9c0d2e2b314efad663a3fcf1cda0af7496f
Sam Mischler <scmischler@outlook.com>
2026-09-24 16:35:05 -0700

    Class refactoring

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import dearpygui.dearpygui as dpg
+from network import NetworkManager
+
+net = NetworkManager()
 
 dpg.create_context()
 dpg.create_viewport(title="Pyrrot", width=1280, height=800, resizable=False)
